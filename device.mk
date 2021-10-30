fa90b40bb8acf2e0420c2d28c3d848b7a0025134
Hariprasad <88080609+hariprasadzx@users.noreply.github.com>
2021-10-30 19:09:11 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -190,3 +190,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+    
+# Device Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
