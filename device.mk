a52c7e7e5e2a3777f97a307948bb11d4c0499f5d
hariprasadzx <hariprasad.r.179@gmail.com>
2021-11-03 19:43:05 +0530

    gauguin: Parts

--- device.mk
+++ device.mk
@@ -57,13 +57,6 @@ PRODUCT_PACKAGES += \
 # A/B
 AB_OTA_UPDATER := false
 
-# Device Settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
-
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
@@ -189,5 +182,11 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
     WifiOverlay
+    
+# Device Settings
 PRODUCT_PACKAGES += \
+    XiaomiParts
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.xiaomiparts.rc \
+    $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.xiaomiparts.rc
