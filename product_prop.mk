3c9256eeab389627bb82dafebad99aeb48bc129f
hariprasadzx <hariprasad.r.179@gmail.com>
2021-11-12 11:53:23 +0530

    gauguin: Disable debug.sf.enable_hwc_vds=0

--- product_prop.mk
+++ product_prop.mk
@@ -38,7 +38,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.latch_unsignaled=0 \
-    #debug.sf.enable_hwc_vds=1 \
+    debug.sf.enable_hwc_vds=0 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # LMKD
